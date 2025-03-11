38c2276be436d39872e223abdd0bcfcf5d4b6ea1
Mark-Eis <mark@markeisler.com>
2025-03-11 14:42:09 +0000

    edit CoordBase.R; issue #89

--- R/CoordBase.R
+++ R/CoordBase.R
@@ -442,7 +442,7 @@ print.waypoints <- function (x, ..., max = NULL) {
         max <- getOption("max.print", 99999L)
     if (!is.finite(max)) 
         stop("invalid 'max' / getOption(\"max.print\"): ", max)
-    omit <- (n0 <- max %/% length(x)) < length(row.names(x))
+    omit <- (n0 <- max %/% 3) < length(row.names(x))
     fmtx <- format(
         if (omit) 
             x[seq_len(n0), , drop = FALSE]
