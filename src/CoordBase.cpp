720aad2118fc5096fcd7885eb4566287aa41fda9
Mark-Eis <mark@markeisler.com>
2025-03-17 22:24:20 +0000

    edit CoordBase.cpp; issue #101

--- src/CoordBase.cpp
+++ src/CoordBase.cpp
@@ -1246,7 +1246,6 @@ NumericVector as_coordswaypoints(DataFrame object, bool which)
 	nv = clone(nv);
 	nv.attr("class") = "coords";
 	nv.attr("fmt") = object.attr("fmt");
-	nv.attr("which") = which ? vector<bool>{ TRUE } : vector<bool>{ FALSE };
 	nv.attr("valid") = object.attr(which ? "validlat" : "validlon");
 	nv.attr("latlon") = which;
 	nv.attr("names") = getnames(object);
