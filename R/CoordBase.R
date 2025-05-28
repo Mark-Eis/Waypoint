0f343ba1e981b20b66fda4a2e8416b332b810bd8
Mark-Eis <mark@markeisler.com>
2025-05-28 08:53:02 +0100

    update CoordBase.R; issue #135

--- R/CoordBase.R
+++ R/CoordBase.R
@@ -771,7 +771,7 @@ review.waypoints <- function(x, ..., show_n = 20L)
 #'
 #' @note
 #' To extract and replace subsets of \code{"\link{waypoints}"} objects, simply use the \pkg{base}
-#' package \code{\link[base:Extract]{`[`}} and \code{\link[base:Extract]{`[<-`}} operators, taking 
+#' package \code{\link[base:Extract]{[}} and \code{\link[base:Extract]{[<-}} operators, taking 
 #' care not to exclude the \code{latitude} and \code{longitude} columns or \code{"Name"} column
 #' (if present), which could lead to undefined results.
 #'
