9e4723e8b6ffa4056788b1f72bbf346fb37251dd
Mark-Eis <mark@markeisler.com>
2026-09-08 13:23:15 +0100

    edit CoordBase.h; issue #223

--- src/CoordBase.h
+++ src/CoordBase.h
@@ -55,55 +55,51 @@ constexpr bool isNumericVector_v = isNumericVector<T>::value;
 
 # if defined __clang__
 	#if (__clang_major__ < 15)
-namespace std
-{
+	namespace std
+	{
 
-	/// Defined in Standard library header <concepts> 
+		/// Concept —— integral (std::integral replicate)
+		template<typename T>
+		concept integral = is_integral_v<T>;
 
-	/// Concept —— integral (std::integral replicate)
-	template<typename T>
-	concept integral = is_integral_v<T>;
+		/// Concept —— floating_point (std::floating_point replicate)
+		template<typename T>
+		concept floating_point = is_floating_point_v<T>;
 
-	/// Concept —— floating_point (std::floating_point replicate)
-	template<typename T>
-	concept floating_point = is_floating_point_v<T>;
+		/// Concept —— derived_from (std::derived_from replicate)
+		template <class _Dp, class _Bp>
+		concept derived_from = is_base_of_v<_Bp, _Dp> && is_convertible_v<const volatile _Dp*, const volatile _Bp*>;
 
-	/// Concept —— derived_from (std::derived_from replicate)
-	template <class _Dp, class _Bp>
-	concept derived_from = is_base_of_v<_Bp, _Dp> && is_convertible_v<const volatile _Dp*, const volatile _Bp*>;
+		/// Concept —— referenceable (std::referenceable instantiation)
+		template<typename T>
+		using __Tref = T&;
 
-	/// Possibly not needed: -
+		template<typename T>
+		concept referenceable = requires() {
+			typename __Tref<T>;
+		};
 
-	/// Defined in header <__type_traits/is_referenceable.h>
+		/// Concept —— dereferenceable (std::dereferenceable instantiation)
+		template <class _Tp>
+		concept dereferenceable = requires(_Tp& __t) {
+		  { *__t } -> referenceable; // not required to be equality-preserving
+		};
 
-	template <class _Tp>
-	concept __referenceable = __is_referenceable_v<_Tp>;
+		/// iter_reference (std::iter_reference_t instantiation)
+		template< dereferenceable T >
+		using iter_reference_t = decltype(*std::declval<T&>());
 
-	/// Defined in header <__iterator/iterator_traits>
-
-	template <class _Tp>
-	concept __dereferenceable = requires(_Tp& __t) {
-	  { *__t } -> __referenceable; // not required to be equality-preserving
-	};
-
-	/// Defined in header <iterator>
-
-	/// iter_reference (std::iter_reference_t replicate)
-	template< dereferenceable T >
-	using iter_reference_t = decltype(*std::declval<T&>());
-
-	/// Concept —— indirectly_writable (std::indirectly_writable replicate)
-	template< class Out, class T >
-	    concept indirectly_writable =
-	        requires(Out&& o, T&& t) {
-	            *o = std::forward<T>(t);
-	            *std::forward<Out>(o) = std::forward<T>(t);
-	            const_cast<const std::iter_reference_t<Out>&&>(*o) = std::forward<T>(t);
-	            const_cast<const std::iter_reference_t<Out>&&>(*std::forward<Out>(o)) =
-	                std::forward<T>(t);
-	        };
-
-}
+		/// Concept —— indirectly_writable (std::indirectly_writable replicate)
+		template< class Out, class T >
+			concept indirectly_writable =
+				requires(Out&& o, T&& t) {
+					*o = std::forward<T>(t);
+					*std::forward<Out>(o) = std::forward<T>(t);
+					const_cast<const iter_reference_t<Out>&&>(*o) = std::forward<T>(t);
+					const_cast<const iter_reference_t<Out>&&>(*std::forward<Out>(o)) =
+						std::forward<T>(t);
+				};
+	}
 	#endif
 #endif
 
